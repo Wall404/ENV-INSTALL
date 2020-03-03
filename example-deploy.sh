2091f64d837e9d5bf5e7ccba8ab15efabdce31c9
Wall404 <wsantillan@huenei.com>
2020-03-03 17:46:02 -0300

    asd

--- example-deploy.sh
+++ example-deploy.sh
@@ -2,5 +2,4 @@
 
 echo -e "\e[34m flink-in-k8s-kind Installation \e[39m"
 git clone https://github.com/jmdacruz/flink-in-k8s-kind.git
-cd flink-in-k8s-kind
-sh deploy.sh
+sh flink-in-k8s-kind/deploy.sh
--- install-enviroment.sh
+++ install-enviroment.sh
@@ -2,19 +2,25 @@
 
 echo -e "\e[34m Enviroment Installation \e[39m"
 
+apt-get update
+apt-get upgrade -y
+
 echo -e "\e[34m Install Docker \e[39m"
 apt install docker.io -y
 systemctl enable docker -y
 
 echo -e "\e[34m Install GO \e[39m"
 wget https://dl.google.com/go/go1.14.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.14.linux-amd64.tar.gz
+tar -C -xzf go1.14.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
+
+sudo apt install golang-go
 
 echo -e "\e[34m Install Kind \e[39m"
 curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname)-amd64
 chmod +x ./kind
-mv ./kind /some-dir-in-your-PATH/kind
+# mv ./kind /some-dir-in-your-PATH/kind
+makekind/
 
 echo -e "\e[34m Install Kubernetes \e[39m"
 apt install curl -y
@@ -30,3 +36,5 @@ swapoff -a
 
 echo -e "\e[34m Run deploy example \e[39m"
 sh example-deploy.sh
+
+#  bcdedit /set hypervisorlaunchtype off
