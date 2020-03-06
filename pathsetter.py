8c9b680cd253dadd2d21e2a7c379c115cb1c5a08
Walter <walter_santillan@hotmail.com>
2020-03-06 11:51:13 -0300

    asd

--- pathsetter.py
+++ pathsetter.py
@@ -5,7 +5,7 @@ import subprocess
 # cmd = "export PATH=$PATH:$HOME/kind/bin/"
 # os.system(cmd)
 
-p = subprocess.Popen(["export", "PATH=$PATH:$HOME/kind/bin/"], stdout=subprocess.PIPE)
+p = subprocess.call(["export", "PATH=$PATH:$HOME/kind/bin/"])
 output, err = p.communicate()
 print(output)
 print("export done")
