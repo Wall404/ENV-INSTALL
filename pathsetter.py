bdf2d0c7344bd6664193f8ed3fc707e2de6f9bb1
Walter <walter_santillan@hotmail.com>
2020-03-06 12:42:35 -0300

    test1.6

--- pathsetter.py
+++ pathsetter.py
@@ -17,8 +17,8 @@ print('new path' + new_path)
 
 os.environ['PATH'] = old_path + new_path
 
-# cmd = "export PATH=$PATH:$HOME/kind/bin/"
-# os.system(cmd)
+cmd = "bash export PATH=$PATH:$HOME/kind/bin/"
+os.system(cmd)
 
 # p = subprocess.call(["export", "PATH=$PATH:$HOME/kind/bin/"])
 # output, err = p.communicate()
